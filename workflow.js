2a0ddab192abff7e46c6dc144d5d69b9bcbd0c6d
Kark Angelo <karkangelovaronapada@gmail.com>
2026-08-16 18:42:53 +0800

    feat: Add workflow script for managing git branches and syncing changes

---
+++ workflow.js
@@ -0,0 +1,18 @@
+const { execSync } = require('child_process')
+
+
+const runCommand = (command) => {
+    try {
+        execSync(command, { stdio: 'inherit' });
+    } catch (e) {
+        console.error(`\n[FAIL] Command failed: "${command}"`);
+        process.exit(1); 
+    }
+};
+
+(async () => {
+    runCommand('git checkout main');
+    runCommand('git pull origin experimental');
+    runCommand('git push origin main');
+    runCommand('git checkout experimental');
+})();
